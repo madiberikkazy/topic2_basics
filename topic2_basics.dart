4236d8640b0711fdc304cb57a4c6730d7ba559a2
Madi Berikkazy <129046203+madiberikkazy@users.noreply.github.com>
2023-09-12 10:12:34 +0600

    Add files via upload

---
+++ topic2_basics.dart
@@ -0,0 +1,126 @@
+import 'dart:math';
+import 'dart:io';
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
+
+void main() {
+// 1)4,5
+  // 2)45
+  // 3)error
+  // 4)2,6
+
+// 1.a)
+  // int x = 2;
+  // int y = 7 * x * 2 - 3 * x + 6;
+  // print(y);
+
+// 1.b)
+  // int a = 2;
+  // int t = 12 * a * 2 + 7 * a - 16;
+  // print(t);
+
+// 2)
+  // int a = 2;
+  // int usti = (a * a + 10);
+  // int asty = a * a + 1;
+  // double tubir = sqrt(asty);
+  // print(usti / tubir);
+
+// 3.a)
+  // double x = 2;
+  // double y = 2;
+  // double z = x * x * x - 2.5 * x * y + 1.78 * x * x - 2.5 * y + 1;
+  // print(z);
+
+// 3.b)
+  // double a = 2;
+  // double b = 2;
+  // double x = 3.56 * a + b * b * b - 5.8 * b * b + 3.8 * a - 1.5;
+  // print(x);
+
+// 4)
+  // double a = 2;
+  // double usti = 2 * a + sin(3 * a);
+  // double bolu = usti / 3.56;
+  // double tubir = sqrt(bolu);
+
+// 5)
+  // print("Sharshynyn uzyndygyn engiz: ");
+  // double uz = double.parse(stdin.readLineSync()!);
+  // double perimeter = 4 * uz;
+  // print("Sharshynyn perimetri $perimeter");
+
+// 6)
+  // int a = 5;
+  // int b = 7;
+  // double arithmeticMean = (a + b) / 2.0;
+  // double geometricMean = sqrt(b * b);
+  // print("Arithmetic Mean: $arithmeticMean");
+  // print("Geometric Mean: $geometricMean");
+
+// 7)
+  // print("San engis: ");
+  // int a = int.parse(stdin.readLineSync()!);
+  // if (a >= 10 && a <= 99) {
+  //   int tens = a ~/ 10;
+  //   int units = a % 10;
+  //   int sumOfDigits = tens + units;
+  //   int productOfDigits = tens * units;
+  //   print("Number of tens: $tens");
+  //   print("Number of units: $units");
+  //   print("Sum of its digits: $sumOfDigits");
+  //   print("Product of its digits: $productOfDigits");
+  // } else {
+  //   print("Qate durys engisiniz.");
+  // }
+
+// 8)
+  // print("Сан енгыз: ");
+  // int a = int.parse(stdin.readLineSync()!);
+  // if (a >= 100 && a <= 999) {
+  //   int hundreds = a ~/ 100;
+  //   int tens = (a % 100) ~/ 10;
+  //   int units = a % 10;
+  //   int sumOfDigits = hundreds + tens + units;
+  //   int productOfDigits = hundreds * tens * units;
+  //   print("Number of hundreds: $hundreds");
+  //   print("Number of tens: $tens");
+  //   print("Number of units: $units");
+  //   print("Sum of its digits: $sumOfDigits");
+  //   print("Product of its digits: $productOfDigits");
+  // } else {
+  //   print("Please enter a valid three-digit number.");
+  // }
+
+// 9)
+  // int number = 123;
+  // int digit3 = number % 10;
+  // int digit2 = (number ~/ 10) % 10;
+  // int digit1 = number ~/ 100;
+  // print("$digit3$digit2$digit1");
+
+// 10)
+  // int number = 123;
+  // int digit3 = number % 10;
+  // int digit2 = (number ~/ 10) % 10;
+  // int digit1 = number ~/ 100;
+  // print("$digit3$digit2$digit1");
+  // print("$digit3$digit1$digit2");
+  // print("$digit2$digit1$digit3");
+  // print("$digit2$digit3$digit1");
+  // print("$digit1$digit2$digit3");
+  // print("$digit1$digit3$digit2");
+
+// 11
+  // int number = 1234;
+  // String numberString = number.toString();
+  // int sum = 0;
+  // int product = 1;
+  // for (int i = 0; i < numberString.length; i++) {
+  //   int digit = int.parse(numberString[i]);
+  //   sum += digit;
+  //   product *= digit;
+  // }
+  // print("Sum of digits: $sum");
+  // print("Product of digits: $product");
+}
